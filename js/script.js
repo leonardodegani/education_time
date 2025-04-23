f949211db99600fb31aa14ddc665fd6dcb17a416
leonardodegani <leonardodegani@gmail.com>
2025-04-23 18:02:39 -0300

    Edit 4

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
+let navbar = document.querySelector('.header .navbar');
 let loginForm = document.querySelector('.login-form');
 
-document.querySelector('#account-btn').onclick = () =>{
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+};
+
+document.querySelector('#account-btn').onclick = () => {
     loginForm.classList.toggle('active');
-}
+    navbar.classList.remove('active');
+};
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+};
+
 
 document.addEventListener("DOMContentLoaded", function () {
     var swiper = new Swiper(".review-slider", {
